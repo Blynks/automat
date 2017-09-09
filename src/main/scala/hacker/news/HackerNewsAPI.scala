c3bdcbd5fa691f6e47231a18cde6ccc2f6092ed7
Eric <eremtek@gmail.com>
2017-09-09 13:02:59 -0400

    working but ugly

--- src/main/scala/hacker/news/HackerNewsAPI.scala
+++ src/main/scala/hacker/news/HackerNewsAPI.scala
@@ -8,16 +8,17 @@ object HackerNewsAPI {
   implicit val formats = DefaultFormats // brings in defaults for case classes
   /**
     * Gets the top N number of stories from Hacker News' API
+    * Default of 30 top stories.
     *
     * @param numberOfStories the desired number of top stories (up to 500)
     * @return Array containing the IDs of N top stories
     */
-  def getHackerNewsTopStoryIDs(numberOfStories: Int = 30): Array[String] = {
+  def getHackerNewsTopStoryIDs(numberOfStories: Int = 30): List[String] = {
     val topStoriesURL: URL = "https://hacker-news.firebaseio.com/v0/topstories.json"
     val apiResponse: JValue = parse(scala.io.Source.fromURL(topStoriesURL).mkString)
-    val listOfTopStoryIDs = apiResponse.extract[Array[String]] // should use case class
+    val topStoryIDs = apiResponse.extract[List[String]]
 
-    listOfTopStoryIDs.slice(0, numberOfStories-1)
+    topStoryIDs.take(numberOfStories)
   }
 
   /**
@@ -34,10 +35,31 @@ object HackerNewsAPI {
   }
 
   def getUserByItemId(id: String): String = {
-    val itemURL: URL = s"https://hacker-news.firebaseio.com/v0/item/$id.json"
-    val apiResponse: JValue = parse(scala.io.Source.fromURL(itemURL).mkString)
-
-    apiResponse.extract[Item].by
+    getItemByID(id).by match {
+      case Some(value: String) => value
+      case _ => "item deleted"
+    }
   }
+
+  def getUserComments(userName: String): List[String] = {
+    userName match {
+      case null => List("No Comments")
+      case _ =>
+        val userURL: URL = s"https://hacker-news.firebaseio.com/v0/user/$userName.json"
+        val apiResponse: JValue = parse(scala.io.Source.fromURL(userURL).mkString)
+        val userProfile: User = apiResponse.extract[User]
+
+        userProfile.submitted
+    }
+
+  }
+
+  def getTopStoriesComments: List[String] = {
+    val topNStoriesIDs = getHackerNewsTopStoryIDs()
+    val listOfStoriesIDs = topNStoriesIDs.map(x => getItemByID(x).kids)
+
+    listOfStoriesIDs.flatten
+  }
+
 
 }
