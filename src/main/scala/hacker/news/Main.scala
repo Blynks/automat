79291ac9af8f832827e3dc3ea8886e67038e550e
Eric <eremtek@gmail.com>
2017-09-11 19:27:30 -0400

    refactoring

--- src/main/scala/hacker/news/Main.scala
+++ src/main/scala/hacker/news/Main.scala
@@ -4,10 +4,9 @@ import HackerNewsAPI._
 import net.liftweb.json._
 import hacker.news.Utils._
 
-import scala.collection.immutable.ListMap
+import scala.collection.parallel.ParMap
 
 /**
-  *
   * Using the Hacker News API Documentation (https://github.com/HackerNews/API)
   * write a program that will print the title of the top 30 hacker news stories
   * and the top 10 commenter names of these stories with the total number of
@@ -20,82 +19,38 @@ object Main extends App {
   implicit val formats = DefaultFormats
   // deal with errors and timeout
   // add parallelism**
-  // if comments are less than 10?
   // control for timeout
-  // recusively search all children items in a thread
-  // get the IDs of the top 30 stories
   // dockerize
   // unit testing
-  val storyIDs = getHackerNewsTopStoryIDs()
+  // use Futures for concurrency
+  val storyIDs = getHackerNewsTopStoryIDs() // default top 30 stories
 
-//   for each of these stories, find the top 10 commenters
-//  for ((x, count)<- storyIDs.zipWithIndex) {
-//    print(s"Story ${count + 1}: ")
-//    val item = getItemByID(x)
-//
-//    item.title match {
-//      case Some(value: String) => println(value)
-//      case _ => println("No Title")
-//    }
-//
-//    val topStoryComments = getTopStoriesComments.toSet
-//    // ONLY CHECKS FIRST 10 FIX THIS
-//    for (y <- getItemByID(x).kids.take(10)) {
-//      val userName = getUserByItemId(y)
-//
-//      userName match {
-//        case "item deleted" => println("deleted comment")
-//        case _ =>
-//          val userSubmitted = getUserComments(userName).toSet
-//          val itemKids = item.kids.toSet
-//
-//
-//          val numberOfPostsInThisThread = userSubmitted.intersect(itemKids).size
-//          val numberOfPostsInTopThreads = userSubmitted.intersect(topStoryComments).size
-//
-//          println(s"$userName (thread score: $numberOfPostsInThisThread, total score: $numberOfPostsInTopThreads)")
-//      }
-//    }
-//  }
-  val testItem = getItemByID("15208138")
-  val testItem2 = getItemByID("15214602")
-
-
-//  commentTraversal(testItem2)
-//  getAllThreadIDs(testItem2).foreach(println)
-
-  val topCommenters = storyIDs
+  val topCommenters: ParMap[String, Int] = storyIDs
     .par
     .map(y => getItemByID(y))
-    .flatMap(x => getAllThreadIDs(x))
-    .map(n => getUserByItemId(n) -> 1)
+    .flatMap(x => getAllChildrenIDs(x))
+    .map(n => getUserByItemID(n) -> 1)
     .groupBy(k => k._1)
     .mapValues(_.map(_._2).sum)
 
   val sortedTopCommenters = topCommenters.toMap
 
+  for ((storyID, storyCount) <- storyIDs.zipWithIndex) {
+    print(s"Story ${storyCount + 1}: ")
+    val storyItem = getItemByID(storyID)
 
-//  topCommenters.foreach(x => println(x))
-//  println(sortedTopCommenters)
-//  sortedTopCommenters.keys.foreach(println)
-//  println(sortedTopCommenters("pmoriarty"))
-
-  for ((x, count)<- storyIDs.zipWithIndex) {
-    print(s"Story ${count + 1}: ")
-    val item = getItemByID(x)
-
-    item.title match {
+    storyItem.title match {
       case Some(value: String) => println(value)
       case _ => println("No Title")
     }
 
-    val allCommentsForThread = getAllThreadIDs(item)
+    val allCommentsForThread = getAllChildrenIDs(storyItem)
     val allUsersAndScores: Map[String, Int] = allCommentsForThread
-      .map(x => getUserByItemId(x) -> 1)
+      .map(x => getUserByItemID(x) -> 1)
       .groupBy(k => k._1)
       .mapValues(_.map(_._2).sum)
 
-    val seq = allUsersAndScores.toSeq.filterNot(x => x._1 == "item deleted")
+    val seq: Seq[(String, Int)] = allUsersAndScores.toSeq.filterNot(x => x._1 == "item deleted")
     val sortedSeq = seq.sortWith(_._2 > _._2).take(10)
     for (x <- sortedSeq) {
       val totalScore = sortedTopCommenters.getOrElse(x._1, 1)
--- src/main/scala/hacker/news/Utils.scala
+++ src/main/scala/hacker/news/Utils.scala
@@ -1,17 +1,15 @@
 package hacker.news
 
+import net.liftweb.json.JsonAST.JValue
+
 
 object  Utils {
   // gives context
   type URL = String
   type StoryName = String
+  type JSON = JValue
 
-  // case classes
+  // case classes for parsing JSON strings
   case class Item(by: Option[String], id: String, kids: List[String], title: Option[String])
   case class User(id: String, submitted: List[String])
-
-
-  // helper function
-
-
 }
